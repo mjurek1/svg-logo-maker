ef7f734c82fadb81691173a356d937d087a168c0
mjurek1 <jurekmichael0@gmail.com>
2024-08-08 14:59:14 -0500

    updates to index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Triangle, Circle, Square} = require("./lib/shapes");
+const { choices } = require('yargs');
 
 const userInput = [
     {
-    
-    }
+    type: "input",
+    name: "name",
+    message: "Enter 3 text characters to appear on the logo",
+    },
+
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape for your logo",
+        choices: ["Triangle", "Circle", "Square"],
+    },
+
+    {
+        type: "input",
+        name: "color",
+        message: "Enter your favorite color",
+        },
 ]
+
